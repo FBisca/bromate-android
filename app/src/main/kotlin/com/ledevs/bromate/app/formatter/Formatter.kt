41e17217e99fa6a68547cb54ef32b200e0d640da
FBisca <felipe15yo@gmail.com>
2017-04-27 22:00:16 -0300

    Creating Formatter

---
+++ app/src/main/kotlin/com/ledevs/bromate/app/formatter/Formatter.kt
@@ -0,0 +1,7 @@
+package com.ledevs.bromate.app.formatter
+
+import java.util.*
+
+interface Formatter {
+  fun dateToMonth(date: Date): String
+}
---
+++ app/src/main/kotlin/com/ledevs/bromate/app/formatter/LocaleFormatter.kt
@@ -0,0 +1,13 @@
+package com.ledevs.bromate.app.formatter
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class LocaleFormatter(locale: Locale) : Formatter {
+
+  private val dateFormat = SimpleDateFormat.getDateInstance(SimpleDateFormat.MEDIUM, locale)
+
+  override fun dateToMonth(date: Date): String {
+    return dateFormat.format(date)
+  }
+}
